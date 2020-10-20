5ea6fa56750cb8f0e56b3b2aed5ea32dfe55dff3
LiJianying <lijy91@foxmail.com>
2020-10-20 22:47:23 +0800

    Add submodules

--- scripts/build_leanflutter_3rd_snippets.sh
+++ scripts/build_leanflutter_3rd_snippets.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
 # 步骤1：编译示例片段（第三方小部件）
-if [ ! -d "/leanflutter_3rd_snippets/" ]; then
-    git clone git@github.com:leanflutter/leanflutter_3rd_snippets.git
-fi
-
 cd leanflutter_3rd_snippets
 git pull
-flutter clean
-flutter build web --profile
+yarn
+node ./scripts/build-3rd-package-examples.js
 
 cd ../
-rm -rf public/3rd_snippets
-cp -R leanflutter_3rd_snippets/build/web public/3rd_snippets
+rm -rf public/3rd_package_examples
+cp -R leanflutter_3rd_snippets/output/3rd_package_examples public/3rd_package_examples
--- scripts/build_leanflutter_snippets.sh
+++ scripts/build_leanflutter_snippets.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 # 步骤1：编译示例片段
-if [ ! -d "/leanflutter_snippets/" ]; then
-    git clone git@github.com:leanflutter/leanflutter_snippets.git
-fi
-
 cd leanflutter_snippets
 git pull
 flutter clean
