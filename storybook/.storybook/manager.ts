64fd3cc4922f3f35a07452edfa005f188b4d6bf5
李健莹 <lijy91@foxmail.com>
2023-12-26 21:36:32 +0800

    chore: Custom brand image

---
+++ storybook/.storybook/manager.ts
@@ -0,0 +1,12 @@
+import { addons } from '@storybook/manager-api';
+import { create } from '@storybook/theming/create';
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'Flutter Widget Livebook',
+    brandUrl: 'https://widget-livebook.leanflutter.dev',
+    brandImage: '/images/brand.png',
+    brandTarget: '_self',
+  }),
+});
