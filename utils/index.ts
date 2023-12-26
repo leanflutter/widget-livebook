b97634bc1f91550b5d7e03034ebdeb9591c40929
李健莹 <lijy91@foxmail.com>
2023-12-26 20:49:21 +0800

    wip: Remove utils

--- utils/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as siteConfig } from './siteConfig';
-export * from './camelToSnake';
-export * from './camelToText';
-export * from './copyToClipboard';
-export { default as gtag } from './gtag';
-export * from './sortString';
-export * from './useCurrentLanguage';
-export * from './useLocalStorage';
-export * from './useMenuVisibility';
--- utils/sortString.ts
+++
@@ -1,8 +0,0 @@
-export const sortString = (a: string, b: string) =>
-  `${a}`
-    .trim()
-    .replace(/[^a-zA-Z ]/g, '')
-    .localeCompare(`${b}`.trim().replace(/[^a-zA-Z ]/g, ''));
-
-export const createSortString = <O>(objectKey: keyof O) => (a: O, b: O) =>
-  sortString(`${a[objectKey]}`, `${b[objectKey]}`);
