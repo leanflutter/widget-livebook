a715aaa47ff7e4e33b519b47540a54a2100ba981
李健莹 <lijy91@foxmail.com>
2023-12-26 20:48:19 +0800

    wip: Remove components

--- components/Ads/index.tsx
+++
@@ -1,7 +0,0 @@
-import CarbonAd from './CarbonAd';
-import GoogleAds1 from './GoogleAds1';
-import GoogleAds2 from './GoogleAds2';
-import WwAds from './WwAds';
-
-const Ads = { CarbonAd, GoogleAds1, GoogleAds2, WwAds };
-export default Ads;
