715d64b2005372ff922c4367b8b2efd01967413e
李健莹 <lijy91@foxmail.com>
2023-12-27 21:47:11 +0800

    chore: Update CircleAvatarDemo

--- storybook/preview_app/lib/demos/circle_avatar/circle_avatar.dart
+++ storybook/preview_app/lib/demos/circle_avatar/circle_avatar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-class CircleAvatarDemo extends StatelessWidget {
-  final String _userAvatarUrl =
-      'https://images.unsplash.com/photo-1565898835704-3d6be4a2c98c?fit=crop&w=200&q=60';
+const String _userAvatarUrl =
+    'https://images.unsplash.com/photo-1565898835704-3d6be4a2c98c?fit=crop&w=200&q=60';
 
+class CircleAvatarDemo extends StatelessWidget {
   const CircleAvatarDemo({super.key});
 
   @override
